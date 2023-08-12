4c768b4a1ec48cab555bcda5d6243365ba50a855
Jvaldes <jvaldes_a@hotmail.com>
2023-08-11 18:06:50 -0600

    tarde 11-08-23

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { AuthLayout } from "./layouts/auth/AuthLayout"
+import { Login } from "./pages/auth/Login"
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-purple-600">Hola Mundo</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Login />} />  
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
