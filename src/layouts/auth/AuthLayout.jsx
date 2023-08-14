50599d790585a58ec6c75092098b9f52ad8144b9
Jvaldes <jvaldes_a@hotmail.com>
2023-08-13 23:45:18 -0600

    Noche 13-08-23

--- src/layouts/auth/AuthLayout.jsx
+++ src/layouts/auth/AuthLayout.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export const AuthLayout = () => {
   return (
-    <div>AuthLayout</div>
+    <div className='bg-red-200 min-h-screen flex items-center justify-center'>
+      AuthLayout <Outlet />
+    </div>
+
   )
 }
